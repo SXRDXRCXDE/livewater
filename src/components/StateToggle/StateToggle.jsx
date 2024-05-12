f0b28abb82366dfc83e8043ffe22acb8600e5274
SXRDXRCXDE <sardorboynazarv@gmail.com>
2024-05-13 01:04:14 +0500

    last

--- src/components/StateToggle/StateToggle.jsx
+++ src/components/StateToggle/StateToggle.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function StateToggle({state}) {
 
     return <>
-        <div className={` ${state==='open'? `text-[#54af57]` : `text-[#dc5d3e]`} p-0.5 text-xs uppercase  `}>
+        <div className={` ${state==='open'? `text-[#54af57] border-[#54af57]` : `text-[#dc5d3e] border-[#dc5d3e]`} border-2 p-0.5 text-xs uppercase  `}>
             {state}
         </div>
     </>
